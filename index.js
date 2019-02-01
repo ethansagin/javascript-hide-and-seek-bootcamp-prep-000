5a29aa64bc7f9eebcf04e86ad0c664ab566b2ed3
Ethan <ethansagin@gmail.com>
2019-02-01 21:23:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  document.getElementById('nested').getElementsByClassName('target');
+}
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('ul.ranked-list li');
+  for(let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (parseInt(lis[i].innerHTML) + n).toString();
+}
+}
+
+function deepestChild() {
+  var current = 'div #grand-node';
+  while(document.querySelector(current) !== undefined) {
+    current = current + ' div'
+  }
+  return document.querySelector(current)
+}
