f476fee77e491c47c2029aa5d8e8f6a3f20a858d
Ethan <ethansagin@gmail.com>
2019-02-01 22:39:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('ul.ranked-list li');
+  for(let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (parseInt(lis[i].innerHTML) + n).toString();
+}
+}
+
+function deepestChild() {
+  var node = document.getElementById('grand-node').querySelectorAll('div');
+  for(let i = 0; i < node.length; i++) {
+    if(node[i].innerHTML.trim() === 'boo!') {
+      return node[i]
+    }
+  }
+}
